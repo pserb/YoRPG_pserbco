c6a464587c876ce1b209b62b64ac4aba94dedb43
joe othman <jothman30@stuy.edu>
2021-11-24 00:33:25 -0500

    now with actual numbers for monster subclasses

--- v6/Witch.java
+++ v6/Witch.java
@@ -1,11 +1,11 @@
 public class Witch extends Monster {
   public Witch() {
-    // health = 150;
-    // strength = 40 + (int)( Math.random() * 45 ); // [40,85)
-    // defense = 20;
-    // attackRating = 1;
+    health = 120;
+    strength = 50 + (int)( Math.random() * 30 ); // [50,80)
+    defense = 27;
+    attackRating = 1;
   }
   public static String about() {
-    return "The witch is the most balanced monster in the game. Decent health, strength, and defense make the witch a formidable monster."
+    return "The witch is the most balanced monster in the game. Decent health, strength, and defense make the witch a formidable monster.";
   }
 }
